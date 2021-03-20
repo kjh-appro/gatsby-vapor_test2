6a441d14aa263f8c95491224fd0092a1901519cc
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-03-20 16:50:51 +0900

    Add files via upload

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,6 +10,17 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+
+
+$(function() { 
+$(window).scroll(function() { 
+  if ($(this).scrollTop() > 250) { 
+  $('#topBtn').fadeIn(); } else { $('#topBtn').fadeOut(); } }); 
+  $("#topBtn").click(function() { $('html, body').animate({ scrollTop : 0 
+  }, 400); }); })
+
+
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
     graphql`
@@ -86,3 +97,123 @@ SEO.propTypes = {
 }
 
 export default SEO
+
+import {
+  Animated,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default class ScrollableHeader extends Component {
+
+  _renderScrollViewContent() {
+    const data = Array.from({length: 30});
+    return (
+      <View style={styles.scrollViewContent}>
+        {data.map((_, i) =>
+          <View key={i} style={styles.row}>
+            <Text>{i}</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+
+  render() {
+    const headerGeight = this.state.scrollY.interpolate({
+        inputRange: [0, HEADER_SCROLL_DISTANCE],
+        outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+        extrapolate: 'clamp',
+    })
+    return (
+      <View style={styles.fill}>
+        <ScrollView
+          style={styles.fill}
+          scrollEventThrottle={16}
+    onScroll={Animated.event(
+      [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
+    )}
+        >
+          {this._renderScrollViewContent()}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  fill: {
+    flex: 1,
+  },
+  row: {
+    height: 40,
+    margin: 16,
+    backgroundColor: '#D3D3D3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
+
+constructor(props)
+
+  super(props);
+
+  this.state = {
+    scrollY: new Animated.Value(0),
+  };
+  
+  const imageOpacity = this.state.scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+    outputRange: [1, 1, 0],
+    extrapolate: 'clamp',
+  });
+  const imageTranslate = this.state.scrollY.interpolate({
+    inputRange: [0, HEADER_SCROLL_DISTANCE],
+    outputRange: [0, -50],
+    extrapolate: 'clamp',
+  });
+<Animated.View style={[styles.header, {height: headerHeight}]}>
+  <Animated.Image
+    style={[
+      styles.backgroundImage,
+      {opacity: imageOpacity, transform: [{translateY: imageTranslate}]},
+    ]}
+    source={require('./images/cat.jpeg')}
+  />
+</Animated.View>
+
+backgroundImage 
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: null;
+  height: HEADER_MAX_HEIGHT;
+  resizeMode: 'cover';
+
+
+header 
+    position: 'absolute';
+    top: 0;
+    left: 0;
+    right: 0;
+    backgroundColor: '#03A9F4';
+    overflow: 'hidden';
+
+bar 
+    marginTop: 28;
+    height: 32;
+    alignItems: 'center';
+    justifyContent: 'center';
+
+title 
+    backgroundColor: 'transparent';
+    color: 'white';
+    fontSize: 18;
+  
+scrollViewContent 
+    marginTop: HEADER_MAX_HEIGHT;
